b65b17c5d2f137a364c258fea50a1727bd000ec0
rtmabanta <111472801+rtmabanta@users.noreply.github.com>
2023-06-22 11:58:08 +0800

    Update User.java

--- User.java
+++ User.java
@@ -5,12 +5,25 @@ public class User {
     private String emailAddress;
     private String password;
     private String studentId;
+    private String address;
+    private int contact;
+    private String contactPname;
+    private String relation;
+    private int contactPnum;
+    private String contactPemail;
     
-    public User(String fullName, String emailAddress, String password, String studentId) {
+    public User(String fullName, String emailAddress, String password, String studentId, String address, 
+    		    int contact, String contactPname, String relation, int contactPnum, String contactPemail) {
         this.fullName = fullName;
         this.emailAddress = emailAddress;
         this.password = password;
         this.studentId = studentId;
+        this.address = address;
+        this.contact = contact;
+        this.contactPname = contactPname;
+        this.relation = relation;
+        this.contactPnum = contactPnum;
+        this.contactPemail = contactPemail;
     
     }
     
@@ -28,6 +41,27 @@ public class User {
 
     public String getStudentId() {
         return studentId;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public String getContactPname() {
+        return contactPname;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+    public int getContactPnum() {
+        return contactPnum;
+    }
+    public String getContactPemail() {
+        return contactPemail;
     }
 }
 
